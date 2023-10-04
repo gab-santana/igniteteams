ba627649d0c4af420e4c46beee5fb34275f3453e
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-10-04 15:07:04 -0300

    add Header Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,9 @@
+import { Header } from '@components/Header';
 import { Container } from './styles'
 export function Groups() {
   return (
     <Container>
+      <Header />
     </Container>
   );
 }
